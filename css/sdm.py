312be675e6725e3b26e51c4047bab85570e0c4a2
Shermit <douglas.amaranto@gmail.com>
2017-10-27 08:28:41 -0500

    qr

---
+++ css/sdm.py
@@ -0,0 +1,57 @@
+db.sdm230.insert_many([
+    {
+        "name": "Voltaje",
+        "unit": None,
+        "value": None},
+    {
+        "name": "Corriente",
+        "unit": None,
+        "value": None},
+    {
+        "name": "Potencia Activa",
+        "unit": None,
+        "value": 3},
+    {
+        "name": "Potencia Aparente",
+        "unit": None,
+        "value": None},
+    {
+        "name": "Potencia Reactiva",
+        "unit": None,
+        "value": None},
+    {
+        "name": "Factor de Potencia",
+        "unit": None,
+        "value": None},
+    {
+        "name": "Angulo de fase",
+        "unit": None,
+        "value": None},
+    {
+        "name": "Frecuencia",
+        "unit": None,
+        "value": None},
+    {
+        "name": "I Energía Activa",
+        "unit": None,
+        "value": None},
+    {
+        "name": "E Energía Activa",
+        "unit": None,
+        "value": None},
+    {
+        "name": "I Energía Reactiva",
+        "unit": None,
+        "value": None},
+    {
+        "name": "E Energía Reactiva",
+        "unit": None,
+        "value": None},
+    {
+        "name": "Energía Activa Total",
+        "unit": None,
+        "value": None},
+    {
+        "name": "Energía Reactiva Total",
+        "unit": None,
+        "value": None}])
